72faa8d5f5b267a39d1b7c2d500f48aa5b9a3bd6
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-11 16:38:15 -0300

    refactored main.js

--- js/main.js
+++ js/main.js
@@ -1,166 +1,185 @@
-const API_URL = 'http://localhost:8080/api/products';
+const API_URL = 'http://localhost:8080/api/products'
 
-const form = document.querySelector('#addForm');
-const editForm = document.querySelector('#editForm');
-const prodList = document.querySelector('.product-list');
-const editSection = document.querySelector('#edit-section');
+const Main = {
+  init: function () {
+    this.getData();
+    this.cacheSelectors();
+    this.bindEvents();
+    Events.eventsCacheSelectors();
+  },
+  
+  cacheSelectors: function () {
+    this.$prodList = document.querySelector('.product-list');
+    this.$addForm = document.querySelector('#addForm');
+    this.$editForm = document.querySelector('#editForm');
+  },
 
+  bindEvents: function() {
+    this.$addForm.onsubmit = this.Events.addForm_submit.bind(this);
+    this.$editForm.onsubmit = this.Events.editForm_submit.bind(this);
+  },
 
-function getData() {
-  fetch(API_URL)
-    .then(res => res.json())
-    .then(data => {
-      let list = '';
+  getData: function () {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        let list = '';
 
-      data.map(product => {
-        return list += `
-          <li>
-            ${product.brand} - ${product.name} - $${product.price} - 
-            <a 
-              href="#" 
-              class="edit-btn" 
-              data-id="${product._id}"
-              data-name="${product.name}"
-              data-brand="${product.brand}"
-              data-price="${product.price}"
-            >
-              [edit]
-            </a>
-            <a 
-              href="#" 
-              class="remove-btn" 
-              data-id="${product._id}"
-            >
-              [remove]
-            </a>
-          </li>
-        `
+        data.map(product => {
+          return list += `
+            <li>
+              ${product.brand} - ${product.name} - $${product.price} - 
+              <a 
+                href="#" 
+                class="edit-button" 
+                data-id="${product._id}"
+                data-name="${product.name}"
+                data-brand="${product.brand}"
+                data-price="${product.price}"
+              >
+                [edit]
+              </a>
+              <a 
+                href="#" 
+                class="remove-btn" 
+                data-id="${product._id}"
+              >
+                [remove]
+              </a>
+            </li>
+          `
+        });
+
+        this.$prodList.innerHTML = list;
+
+        Events.editBtn_click();
+        Events.removeBtn_click();
+        
       });
+  },
 
-      prodList.innerHTML = list;
-
-      editBtnEvent();
-      removeBtnEvent();
-      
-    });
-};
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const name = document.forms['addForm'].name.value;
-  const brand = document.forms['addForm'].brand.value;
-  const price = document.forms['addForm'].price.value;
-
-
-  fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name,
-      brand,
-      price,
-    }),
-  }).then(res => {
-    res.json().then(data => {
-      if (data.message === 'success') {
-        form.reset()
-        getData();
-        alert('Cadastro relizado com sucesso');
-      } else {
-        alert('Ops, deu algum erro, tente novamente');
-      }
-    })
-  });
-
-});
-
-editForm.onsubmit = function(e) {
-  e.preventDefault();
-
-  const id = document.forms['editForm'].id.value;
-  const name = document.forms['editForm'].name.value;
-  const brand = document.forms['editForm'].brand.value;
-  const price = document.forms['editForm'].price.value;
-
-  console.log(id)
-  console.log(name)
-  console.log(brand)
-  console.log(price)  
-
-  fetch(`${API_URL}/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name,
-      brand,
-      price,
-    }),
-  }).then(res => res.json())
-    .then(data => {
-      if(data.message === 'success'){
-        form.reset();
-        getData();
-        editSection.classList.add('hidden');
-        alert('produto editado com sucesso');
-      } else {
-        alert('Ops, algo de errado, tente novamente');
-      };
-    });
-
-};
-
-function editBtnEvent() {
-  const editBtn = document.querySelectorAll('.edit-btn');
-
-  editBtn.forEach(btn => {
-    btn.onclick = function(e) {
+  Events: {
+    addForm_submit: function (e) {
       e.preventDefault();
-      editSection.classList.remove('hidden');
+  
+      const name = document.forms['addForm'].name.value;
+      const brand = document.forms['addForm'].brand.value;
+      const price = document.forms['addForm'].price.value;
+  
+  
+      fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          brand,
+          price,
+        }),
+      }).then(res => {
+        res.json().then(data => {
+          if (data.message === 'success') {
+            this.$addForm.reset()
+            Main.getData();
+            alert('Cadastro relizado com sucesso');
+          } else {
+            alert('Ops, deu algum erro, tente novamente');
+          }
+        })
+      });
+  
+    },
 
-      const id = this.dataset.id;
-      const name = this.dataset.name;
-      const brand = this.dataset.brand;
-      const price = this.dataset.price;
-
-      document.forms['editForm'].id.value = id;
-      document.forms['editForm'].name.value = name;
-      document.forms['editForm'].brand.value = brand;
-      document.forms['editForm'].price.value = price;
-
-    };
-
-  });
-};
-
-function removeBtnEvent() {
-  const removeBtn = document.querySelectorAll('.remove-btn');
-
-  removeBtn.forEach(btn => {
-    btn.onclick = function(e) {
+    editForm_submit: function (e) {
       e.preventDefault();
-      
-      
-      const id = this.dataset.id;
-      
+  
+      const id = document.forms['editForm'].id.value;
+      const name = document.forms['editForm'].name.value;
+      const brand = document.forms['editForm'].brand.value;
+      const price = document.forms['editForm'].price.value;
+  
       fetch(`${API_URL}/${id}`, {
-        method: 'DELETE',
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          brand,
+          price,
+        }),
       }).then(res => res.json())
         .then(data => {
-          if(data.message === 'success'){
-            getData();
-            alert('Cadastro removid com sucesso');
+          if (data.message === 'success') {
+            this.$editForm.reset();
+            this.getData();
+            Events.$editSection.classList.add('hidden');
+            alert('produto editado com sucesso');
           } else {
-            alert('Ops, ocorreu um erro');
+            alert('Ops, algo de errado, tente novamente');
           };
-      });
+        });
+  
+    },
 
-    };
-  });
+  },
 };
 
-getData();
+
+const Events = {
+  eventsCacheSelectors: function(){
+    this.$editSection = document.querySelector('#edit-section');
+    this.$editBtn = document.querySelectorAll('.edit-button');
+  },
+  
+  editBtn_click: function (e) {
+    const editBtn = document.querySelectorAll('.edit-button');
+
+    editBtn.forEach(btn => {
+      btn.onclick = function (e) {
+        e.preventDefault();
+        Events.$editSection.classList.remove('hidden');
+
+        const id = this.dataset.id;
+        const name = this.dataset.name;
+        const brand = this.dataset.brand;
+        const price = this.dataset.price;
+
+        document.forms['editForm'].id.value = id;
+        document.forms['editForm'].name.value = name;
+        document.forms['editForm'].brand.value = brand;
+        document.forms['editForm'].price.value = price;
+
+      };
+
+    });
+  },
+
+  removeBtn_click: function (e) {
+    const removeBtn = document.querySelectorAll('.remove-btn');
+
+    removeBtn.forEach(btn => {
+      btn.onclick = function (e) {
+        e.preventDefault();
+
+        const id = this.dataset.id;
+
+        fetch(`${API_URL}/${id}`, {
+          method: 'DELETE',
+        }).then(res => res.json())
+          .then(data => {
+            if (data.message === 'success') {
+              Main.getData();
+              alert('Cadastro removid com sucesso');
+            } else {
+              alert('Ops, ocorreu um erro');
+            };
+          });
+
+      };
+    });
+  }
+};
+
+Main.init();

6065b90e9686d35ee8be9d892721490247c359d7
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-11 11:41:01 -0300

    add method put

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const API_URL = 'http://localhost:8080/api/products';
 
-const form = document.querySelector('#inputForm');
+const form = document.querySelector('#addForm');
+const editForm = document.querySelector('#editForm');
 const prodList = document.querySelector('.product-list');
+const editSection = document.querySelector('#edit-section');
 
 
 function getData() {
@@ -13,35 +15,32 @@ function getData() {
       data.map(product => {
         return list += `
           <li>
-           ${product.brand} - ${product.name} - $${product.price} - 
-           <a href="#" class="remove-btn" data-id="${product._id}">[excluir]</a>
+            ${product.brand} - ${product.name} - $${product.price} - 
+            <a 
+              href="#" 
+              class="edit-btn" 
+              data-id="${product._id}"
+              data-name="${product.name}"
+              data-brand="${product.brand}"
+              data-price="${product.price}"
+            >
+              [edit]
+            </a>
+            <a 
+              href="#" 
+              class="remove-btn" 
+              data-id="${product._id}"
+            >
+              [remove]
+            </a>
           </li>
         `
       });
 
       prodList.innerHTML = list;
 
-      const removeBtn = document.querySelectorAll('.remove-btn');
-      removeBtn.forEach(btn => {
-        btn.onclick = function(e) {
-          e.preventDefault();
-
-          const id = this.dataset.id;
-
-          fetch(`${API_URL}/${id}`, {
-            method: 'DELETE',
-          })
-            .then(res => res.json())
-            .then(data => {
-              if(data.message === 'success'){
-                getData();
-                alert('Cadastro removid com sucesso');
-              } else {
-                alert('Ops, ocorreu um erro');
-              };
-            });
-        };
-      });
+      editBtnEvent();
+      removeBtnEvent();
       
     });
 };
@@ -49,9 +48,9 @@ function getData() {
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const name = document.forms['inputForm'].name.value;
-  const brand = document.forms['inputForm'].brand.value;
-  const price = document.forms['inputForm'].price.value;
+  const name = document.forms['addForm'].name.value;
+  const brand = document.forms['addForm'].brand.value;
+  const price = document.forms['addForm'].price.value;
 
 
   fetch(API_URL, {
@@ -77,5 +76,91 @@ form.addEventListener('submit', e => {
   });
 
 });
+
+editForm.onsubmit = function(e) {
+  e.preventDefault();
+
+  const id = document.forms['editForm'].id.value;
+  const name = document.forms['editForm'].name.value;
+  const brand = document.forms['editForm'].brand.value;
+  const price = document.forms['editForm'].price.value;
+
+  console.log(id)
+  console.log(name)
+  console.log(brand)
+  console.log(price)  
+
+  fetch(`${API_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+      brand,
+      price,
+    }),
+  }).then(res => res.json())
+    .then(data => {
+      if(data.message === 'success'){
+        form.reset();
+        getData();
+        editSection.classList.add('hidden');
+        alert('produto editado com sucesso');
+      } else {
+        alert('Ops, algo de errado, tente novamente');
+      };
+    });
+
+};
+
+function editBtnEvent() {
+  const editBtn = document.querySelectorAll('.edit-btn');
+
+  editBtn.forEach(btn => {
+    btn.onclick = function(e) {
+      e.preventDefault();
+      editSection.classList.remove('hidden');
+
+      const id = this.dataset.id;
+      const name = this.dataset.name;
+      const brand = this.dataset.brand;
+      const price = this.dataset.price;
+
+      document.forms['editForm'].id.value = id;
+      document.forms['editForm'].name.value = name;
+      document.forms['editForm'].brand.value = brand;
+      document.forms['editForm'].price.value = price;
+
+    };
+
+  });
+};
+
+function removeBtnEvent() {
+  const removeBtn = document.querySelectorAll('.remove-btn');
+
+  removeBtn.forEach(btn => {
+    btn.onclick = function(e) {
+      e.preventDefault();
+      
+      
+      const id = this.dataset.id;
+      
+      fetch(`${API_URL}/${id}`, {
+        method: 'DELETE',
+      }).then(res => res.json())
+        .then(data => {
+          if(data.message === 'success'){
+            getData();
+            alert('Cadastro removid com sucesso');
+          } else {
+            alert('Ops, ocorreu um erro');
+          };
+      });
+
+    };
+  });
+};
 
 getData();
